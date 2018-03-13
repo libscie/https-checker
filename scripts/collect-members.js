8e80c10ff55ca04fda559ab771f522af648c8639
Chris Hartgerink <chris@libscie.org>
2018-03-13 23:28:25 +0100

    End of day commit.
    I am currently trying to get all raw JSON from NodeJS, but for some reason this is much tougher than expected (the script stops running for some reason). Considering whether I should just create a down and dirty script in bash, which works super fast and I can write it in a jiffy. Ah well, things for another day.

--- scripts/collect-members.js
+++ scripts/collect-members.js
@@ -1,10 +1,41 @@
 #!/bin/node
 
-let base_cr = 'https://api.crossref.org/'
-// let base_dc = 'https://api.datacite.org/'
+const https = require('https')
+const sleep = require('sleep')
+const fs = require('fs')
 
-// while not last page
-// query page
-// collect all prefixes
-// collect total-dois
-// append to file
+let base = 'https://api.crossref.org/members'
+let id = 'mailto=info@libscie.org'
+let pageLimit = 1000
+
+let limit, interval, nrMembers
+// get headers
+https.get(`${base}?rows=0&${id}`, (req, err) => {
+  if (err) throw err
+  limit = parseInt(req.headers['x-rate-limit-limit'])
+  interval = parseInt(req.headers['x-rate-limit-interval'].replace(/s/g, ''))
+  req.on('data', (d) => {
+    nrMembers = JSON.parse(d.toString()).message['total-results']
+  })
+})
+
+// let i = 1
+let off = 0
+
+https.get(`${base}?rows=${pageLimit}&${id}&offset=${off}`, (req, err) => {
+  if (err) throw err
+  let body = ''
+  req.on('data', (d) => {
+    // console.log(Object(d.toString()))
+    body = body.concat(d.toString())
+    // console.log(body)
+    // write away json
+    fs.writeFile(`db/members_${off}.json`, body, 'utf8', (err) => {
+      if (err) throw err
+      console.log(`Successfully collected for members ${off}-${off + pageLimit}.`)
+    })
+  })
+})
+
+off += 1000
+sleep.sleep(1)
